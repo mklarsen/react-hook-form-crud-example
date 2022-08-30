eda90ac74b8ac0a8cc1d325705fd2fce0a36878b
Martin Kraus Larsen <26963547+mklarsen@users.noreply.github.com>
2022-08-30 13:58:44 +0200

    Update List.jsx
    working

--- src/domainentries/List.jsx
+++ src/domainentries/List.jsx
@@ -5,25 +5,25 @@ import { domainEntrieService } from '@/_services';
 
 function List({ match }) {
     const { path } = match;
-    const [domains, setDomainEntrie] = useState(null);
+    const [entries, setDomainEntrie] = useState(null);
 
     useEffect(() => {
         domainEntrieService.getAll().then(x => setDomainEntrie(x));
     }, []);
 
     function deleteDomainEntrie(id) {
-        setDomainEntrie(domains.map(x => {
+        setDomainEntrie(entries.map(x => {
             if (x.id === id) { x.isDeleting = true; }
             return x;
         }));
         domainEntrieService.delete(id).then(() => {
-            setDomainEntrie(domains => domains.filter(x => x.id !== id));
+            setDomainEntrie(entries => entries.filter(x => x.id !== id));
         });
     }
 
     // prepared for zone entries
     function editDomainEntrie(id) {
-        setDomainEntrie(domains.map(x => {
+        setDomainEntrie(entries.map(x => {
             if (x.id === id) { x.isEditing = true; }
             return x;
         }));
@@ -40,48 +40,49 @@ function List({ match }) {
             <table className="table table-striped">
                 <thead>
                     <tr>
-                        <th style={{ width: '10%' }}>id</th>
+                        <th style={{ width: '3%' }}>id</th>
                         <th style={{ width: '20%' }}>Domain</th>
-                        <th style={{ width: '10%' }}></th>
+                        <th style={{ width: '10%' }}>Name</th>
+                        <th style={{ width: '10%' }}>TTL</th>
+                        <th style={{ width: '10%' }}>TYPE</th>
+                        <th style={{ width: '10%' }}>record</th>
+                        <td></td>
                     </tr>
                 </thead>
                 <tbody>
-                    {domains && domains.map(domain =>
-                        <tr key={domain.id}>
-                            <td>{domain.domhand}</td>
-                            <td>{domain.domain}</td>
+                    {entries && entries.map(entrie =>
+                        <tr key={entrie.id}>
 
+                            <td>{entrie.id}</td>
+                            <td>{entrie.domain}</td>
+                            <td>{entrie.name}</td>
+                            <td>{entrie.ttl}</td>
+                            <td>{entrie.type}</td>
+                            <td>{entrie.record}</td>
+                            
                             <td style={{ whiteSpace: 'nowrap' }}>
-                                <Link to={`${path}/edit/${domain.id}`} className="btn btn-sm btn-primary mr-1">Edit</Link>
-                                <button onClick={() => deleteDomain(domain.id)} className="btn btn-sm btn-danger btn-delete-user" disabled={domain.isDeleting}>
-                                    {domain.isDeleting
+                                <Link to={`${path}/edit/${entrie.id}`} className="btn btn-sm btn-primary mr-1">Edit</Link>
+                                <button onClick={() => deleteDomain(entrie.id)} className="btn btn-sm btn-danger btn-delete-user" disabled={entrie.isDeleting}>
+                                    {entrie.isDeleting
                                         ? <span className="spinner-border spinner-border-sm"></span>
                                         : <span>Delete</span>
                                     }
                                 </button>
 
-                                <button onClick={() => editDomain(domain.id)} className="btn btn-sm btn-outline-info" disabled={domain.isEditing}>
-                                    {domain.isEditing
-                                        ? <span className="spinner-border spinner-border-sm"></span>
-                                        : <span>Configure</span>
-                                    }
-                                </button>
-
-
                             </td>
                         </tr>
                     )}
-                    {!domains &&
+                    {!entries &&
                         <tr>
                             <td colSpan="4" className="text-center">
                                 <div className="spinner-border spinner-border-lg align-center"></div>
                             </td>
                         </tr>
                     }
-                    {domains && !domains.length &&
+                    {entries && !entries.length &&
                         <tr>
                             <td colSpan="4" className="text-center">
-                                <div className="p-2">No Domains to Display</div>
+                                <div className="p-2">No entries to Display</div>
                             </td>
                         </tr>
                     }
